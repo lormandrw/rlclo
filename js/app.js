aeb7845655fd7934c1b00fd3e7dbcdcaa1d3214c
lormandrw <lormandrw@yahoo.com>
2015-12-25 15:26:45 -0600

    First Check In

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(function(){
+  $("#menu").load("menu.html");
+}); 
+
+$(document).on('click', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a')) {
+        $(this).collapse('hide');
+    }
+});
